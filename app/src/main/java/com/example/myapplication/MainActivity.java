5057ca3d2ab3db72d1e685dab5538876280ae34f
mjk0150 <79957757+mjk0150@users.noreply.github.com>
2021-09-13 13:10:58 +0900

    button_image_and_build.gradle

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -4,9 +4,12 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    private ActivityExample1Binding binidng;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,6 +18,8 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void onClick(View view) {
+        ImageView imageView = findViewById(R.id.imageView);
+        imageView.setImageResource(R.drawable.elmo);
         Toast.makeText(this, "안녕하세요 버튼 클릭입니다", Toast.LENGTH_LONG).show();
     }
 }
