8ba98b4bebc0a872a8c6cfd94e68d031e10a11fc
mjk0150 <79957757+mjk0150@users.noreply.github.com>
2021-09-20 00:56:00 +0900

    bert로 이미지 변경 및 binding

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -4,22 +4,23 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
-import android.widget.ImageView;
 import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
-    private ActivityExample1Binding binidng;
+import com.example.myapplication.databinding.ActivityMainBinding;
 
+public class MainActivity extends AppCompatActivity {
+    private ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        View view = binding.getRoot();
+        setContentView(view);
     }
 
     public void onClick(View view) {
-        ImageView imageView = findViewById(R.id.imageView);
-        imageView.setImageResource(R.drawable.elmo);
-        Toast.makeText(this, "안녕하세요 버튼 클릭입니다", Toast.LENGTH_LONG).show();
+        binding.imageView.setImageResource(R.drawable.bert);
+        Toast.makeText(this,"안녕하세요, 버튼 클릭입니다.", Toast.LENGTH_LONG).show();
     }
 }
