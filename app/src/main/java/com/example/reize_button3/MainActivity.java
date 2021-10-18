05a89443ba2f06973117b70b18cf7891cc11b48e
mjk0150 <79957757+mjk0150@users.noreply.github.com>
2021-10-18 10:58:40 +0900

    resize buttons - to lambda

---
+++ app/src/main/java/com/example/reize_button3/MainActivity.java
@@ -0,0 +1,54 @@
+package com.example.reize_button3;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
+
+import android.os.Bundle;
+import android.view.View;
+
+import com.example.reize_button3.databinding.ActivityMainBinding;
+
+public class MainActivity extends AppCompatActivity {
+    ActivityMainBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        binding.button100.setOnClickListener(this::buttonEvent);
+        binding.button200.setOnClickListener(this::buttonEvent);
+        binding.button400.setOnClickListener(this::buttonEvent);
+    }
+
+    private void buttonEvent(View v) {
+        int newHeight = 0;
+
+        switch (v.getId()) {
+            case R.id.button100:
+                newHeight = 100;
+                break;
+
+            case R.id.button200:
+                newHeight = 200;
+                break;
+
+            case R.id.button400:
+                newHeight = 400;
+                break;
+        }
+
+        setImageHeight(newHeight);
+    }
+
+    private void setImageHeight(float newHeight) {
+        ConstraintLayout.LayoutParams param = (ConstraintLayout.LayoutParams) binding.image.getLayoutParams();
+        param.height = dp2pixel(newHeight);
+        binding.image.setLayoutParams(param);
+    }
+
+    private int dp2pixel(float dp) {
+        return (int) (dp * getResources().getDisplayMetrics().density);
+    }
+}
